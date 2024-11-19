375c29eec07edc9e43618b3c78aeecc833d6c927
Semi Akhigbe <akhigbejoy5@gmail.com>
2024-11-19 00:07:55 +0000

    Created essence reserve project and app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn essence_reserve.wsgi
---
+++ dine_essence/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DineEssenceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dine_essence'

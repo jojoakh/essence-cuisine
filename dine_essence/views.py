32443c21cf88f6588cea31cec5f4895ddc45e748
Semi Akhigbe <akhigbejoy5@gmail.com>
2024-11-20 02:38:57 +0000

    Add index.html and about.html templates

--- dine_essence/views.py
+++ dine_essence/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello, Django!")
+def index(request):
+    return render(request, 'dine_essence/index.html')
+
+
+def about(request):
+    return render(request, 'dine_essence/about.html')
+
+
+def book_table(request):
+    if request.method == "POST":
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = ReservationForm()
+    return render(request, 'book_table.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'success.html')

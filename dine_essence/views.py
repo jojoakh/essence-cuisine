8d874ae5c83d1712f4ad81214b35467a49a4ee40
Semi Akhigbe <akhigbejoy5@gmail.com>
2024-11-23 15:36:53 +0000

    Create base.html template, and add immage to home page

--- dine_essence/views.py
+++ dine_essence/views.py
@@ -10,16 +10,3 @@ def about(request):
     return render(request, 'dine_essence/about.html')
 
 
-def book_table(request):
-    if request.method == "POST":
-        form = ReservationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('success')
-    else:
-        form = ReservationForm()
-    return render(request, 'book_table.html', {'form': form})
-
-
-def success(request):
-    return render(request, 'success.html')
--- essence_reserve/settings.py
+++ essence_reserve/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dine_essence',
+        
 ]
 
 MIDDLEWARE = [

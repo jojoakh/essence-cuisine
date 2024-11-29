aac67d4b34534b66a25ce78e7c96f02d89752cb8
Semi Akhigbe <akhigbejoy5@gmail.com>
2024-11-29 11:02:14 +0000

    Add opening hours to index.html and about.html

--- essence_reserve/settings.py
+++ essence_reserve/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3e%q(1(0az8j6*8qi!j0t7%zp+qf7ta@y-5hj&bjbqhjcm1+u3
 SECRET_KEY = os.environ.get("SECRET_KEY", "f4745f257adb1e71919a217933564d1f")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-jojoakh-essencecuisine-qlbduc49ssw.ws.codeinstitute-ide.net", ".herokuapp.com"]
 
@@ -39,7 +39,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://your-heroku-app.herokuapp.com'
 ]
 
-
+ 
 # Application definition
 
 INSTALLED_APPS = [
